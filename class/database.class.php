1f6e1a117226e051fe57e94b74e233d727cd89d1
Ian Patrick <56745915+ianpatricck@users.noreply.github.com>
2020-04-18 18:01:06 -0300

    UT8 encoding
    New default attribute to connection

--- class/database.class.php
+++ class/database.class.php
@@ -8,7 +8,7 @@ class Connection
     {
         try
         {
-            $this->connection = new PDO("mysql:host=$host", $user, $pass);
+            $this->connection = new PDO("mysql:host=$host", $user, $pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
             $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
         } catch (PDOException $error) {
