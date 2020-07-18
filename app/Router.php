f3bf28cdf42ffacf565260a38c066ff4467486cd
Ian Patrick <56745915+ianpatricck@users.noreply.github.com>
2020-07-17 23:44:25 -0300

    URL

--- app/Router.php
+++ app/Router.php
@@ -1,8 +1,20 @@
 <?php
 
+use App\Codew\URL;
+
 function route($route, $param)
 {
     # In development ...
+
+    /*
+    
+    $views = URL::call([
+        'init' => 'init'
+    ]);
+    
+    URL::index($views);
+
+    */
 }
 
 ?>
--- index.php
+++ index.php
@@ -2,10 +2,4 @@
 
 require __DIR__ . '/app.php';
 
-use App\Codew\URL;
-
-$views = URL::call([
-    'init' => 'init'
-]);
-
-URL::index($views);
+# ...
