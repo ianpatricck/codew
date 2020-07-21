f3f0396dca5b6db946ef5c3cf2ea8d811c73acb7
Ian Patrick <56745915+ianpatricck@users.noreply.github.com>
2020-07-21 18:06:49 -0300

    Return config folder

---
+++ config/db.config.php
@@ -0,0 +1,8 @@
+<?php
+
+const HOST = 'localhost';
+const DB_NAME = 'codeworker';
+const USERNAME = 'root';
+const PASSWORD = '';
+
+?>
---
+++ config/url.config.php
@@ -0,0 +1,6 @@
+<?php
+
+const INDEX_FOLDER = 'resources/urls/';
+const INDEX_VIEW = 'init';
+
+?>
