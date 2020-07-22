96ba763ebfb2dc77637db365181ca93fba4aa158
Ian Patrick <56745915+ianpatricck@users.noreply.github.com>
2020-07-22 15:27:24 -0300

    Notes md

--- index.php
+++ index.php
@@ -1,8 +1,3 @@
 <?php
 
 require __DIR__ . '/app.php';
-
-# [-] Fazer com que o controlador manipule o banco de dados e devolva uma view.
-# [-] Obstruir o arquivo <except.php> para a manipulação de dados.
-# [-] Criar (se possivel) um sistema de rotas.
-# [-] Automatizar CRUD em CLI.
