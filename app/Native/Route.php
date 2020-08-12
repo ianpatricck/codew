60cdb5b82f858dbe11f86a2aa3cae1b5a268b040
Ian Patrick <56745915+ianpatricck@users.noreply.github.com>
2020-08-12 00:30:51 -0300

    RUN CLI

--- app/Native/Route.php
+++ app/Native/Route.php
@@ -6,21 +6,15 @@ class Route
 {
     public static function controller($view, $name, $method)
     {
-        $file_controller = file_get_contents('app/Controllers/' . $name . '.php');
-
-        $file_controller_cache = fopen('interpreters/cache/controllers/'. $name .'.cache', 'w');
-
-        fwrite($file_controller_cache, $file_controller);
-        fclose($file_controller_cache);
-
-        $file_controller_new = file('interpreters/cache/controllers/'. $name .'.cache');
+        $file_controller_new = file('app/Controllers/' . $name . '.php');
         
         foreach ($file_controller_new as $line) {
             $line = '';
+            break;
         }
 
         foreach ($file_controller_new as $line) {
-            $content = "<pre>". $line ."</pre>";
+            $content = "<code>". $line ."</code>";
             echo $content;
         }
     }
---
+++ interpreters/cli.php
@@ -0,0 +1,43 @@
+<?php
+
+require_once __DIR__ . '/../config/define.php';
+
+/* 
+* ------------------------------------------
+* ----------------------------- :: Server
+* ------------------------------------------ 
+*/
+
+function start($port = PORT)
+{
+    echo "Starting codeworker PHP server ...\n";
+    echo exec('php -S localhost:' . $port);
+}
+
+/* 
+* ------------------------------------------
+* ----------------------------- :: Controllers
+* ------------------------------------------ 
+*/
+
+
+function createController($name)
+{
+    $content = "<?php
+
+namespace App\Controllers;
+
+class $name
+{
+    // ..
+}
+
+";
+
+    $file_controller = fopen('app/Controllers/'.$name.'.php', 'w');
+
+    fwrite($file_controller, $content);
+    fclose($file_controller);
+
+    echo "\n[+] Controller created successfully\n";
+}
