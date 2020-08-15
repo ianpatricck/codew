9639796de116d539222463c91cacda640ee018f5
Ian Patrick <56745915+ianpatricck@users.noreply.github.com>
2020-08-15 20:47:51 -0300

    removed URL config

--- config/define.php
+++ config/define.php
@@ -10,8 +10,3 @@ define('HOST', '127.0.0.1');
 define('DB_NAME', 'codeworker');
 define('USERNAME', 'root');
 define('PASSWORD', '');
-
-// => URL config
-
-define('INDEX_FOLDER', 'resources/views/');
-define('INDEX_VIEW', 'home');
