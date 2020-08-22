d642751ec23ff06235af1d1c1d3cce00c03fbee6
Ian Patrick <56745915+ianpatricck@users.noreply.github.com>
2020-08-22 00:50:10 -0300

    Hide CRUD 2

--- index.php
+++ index.php
@@ -3,7 +3,11 @@
 require __DIR__ . '/codew.php';
 
 use App\Controllers\WelcomeController;
+use App\Controllers\Crud;
 
 /* ------------------- */
 
 WelcomeController::viewer();
+
+Crud::view();
+Crud::register('register');
---
+++ view/crud/register.view.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register | CRUD</title>
+
+    <link rel="stylesheet" href="<?php staticFile('css/crud.css'); ?>">
+</head>
+<body>
+
+    <div class="container">
+        <div class="inline-text">
+            <p class="title">codeworker ::</p> &nbsp;
+            <h2>Register</h2>
+        </div>
+        <form action="/register" method="POST">
+            <input type="text" name="name" placeholder="Name">
+            <input type="text" name="email" placeholder="E-mail">
+            <input type="password" name="password" placeholder="Password">
+            <input type="password" name="password_confirm" placeholder="Confirm password">
+
+            <button type="submit">Register</button>
+        </form>
+
+        <a href="login">Login</a>
+    </div>
+</body>
+</html>
