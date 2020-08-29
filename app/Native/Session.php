50146db234ae70387fd265c9e520e3c93830b975
Ian Patrick <56745915+ianpatricck@users.noreply.github.com>
2020-08-29 18:45:12 -0300

    Session methods dev

--- app/Native/Session.php
+++ app/Native/Session.php
@@ -4,5 +4,15 @@ namespace App\Native;
 
 class Session
 {
-    // ..
+    public static function obj($obj)
+    {
+        foreach ($obj as $value) {
+            // ..
+        }
+    }
+
+    public static function data($obj, $data)
+    {
+        // ..
+    }
 }
