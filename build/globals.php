ae9f4c2dcda2ef8b2b023af64a0777efbcc664bb
Ian Patrick <56745915+ianpatricck@users.noreply.github.com>
2020-09-09 16:18:42 -0300

    Finished push function

--- build/globals.php
+++ build/globals.php
@@ -11,7 +11,19 @@ function view($page, $data = [])
 
 function push($view, $param)
 {
-    // ..
+    if ($_SERVER['REQUEST_URI'] == '/' . $view . '/') {
+        echo false;
+    } else if ($_SERVER['REQUEST_URI'] == '/' . $view . '/' . $param) {
+        $value = $_SERVER['REQUEST_URI'];
+        
+        $array_URL = explode('/', $value);
+
+        foreach ($array_URL as $key => $value) {}
+
+        if (in_array($param, $array_URL)) {
+            require_once __DIR__ . '/../view/' . $view . '.view.php';
+        }
+    }
 }
 
 function redirect($page)
