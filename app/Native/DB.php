4d3210eacf9cac0fa4645ce2d92bd5b341337fbc
Ian Patrick <56745915+ianpatricck@users.noreply.github.com>
2020-09-13 10:18:42 -0300

    DB __construct()

--- app/Native/DB.php
+++ app/Native/DB.php
@@ -9,29 +9,26 @@ class DB
 {
     private $connection;
 
-    public function mysql()
+    public function __construct($database)
     {
-        try {
-            $this->connection = new PDO('mysql:host='.HOST.';dbname='.DB_NAME, USERNAME, PASSWORD, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $this->connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
-        } catch (PDOException $error) {
-            throw new PDOException($error);
+        if ($database == 'mysql') {
+            try {
+                $this->connection = new PDO('mysql:host='.HOST.';dbname='.DB_NAME, USERNAME, PASSWORD, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+                $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $this->connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+            } catch (PDOException $error) {
+                throw new PDOException($error);
+            }
+        } else if ($database == 'pgsql') {
+            try {
+                $this->$connection = new PDO('pgsql:host='.HOST.';dbname='.DB_NAME, USERNAME, PASSWORD);
+                $this->$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $this->connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+            } catch (PDOException $error) {
+                throw new PDOException($error);
+            }
         }
     }
-
-    public function pgsql()
-    {
-        try {
-            $this->$connection = new PDO('pgsql:host='.HOST.';dbname='.DB_NAME, USERNAME, PASSWORD);
-            $this->$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $this->connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
-        } catch (PDOException $error) {
-            throw new PDOException($error);
-        }
-    }
-
-    // => Queries
 
     public function InsertQuery($query)
     {
