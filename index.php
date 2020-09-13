1da3cd0f89a7f00cbe36f7f61dba1e7ad2c5115f
Ian Patrick <56745915+ianpatricck@users.noreply.github.com>
2020-09-13 10:42:31 -0300

    Finished Complex

--- index.php
+++ index.php
@@ -3,14 +3,5 @@
 require __DIR__ . '/codew.php';
 
 use App\Controllers\WelcomeController;
-use App\Controllers\Complex;
 
 WelcomeController::viewer();
-
-Complex::viewer();
-Complex::register();
-Complex::login();
-Complex::logout();
-Complex::show();
-Complex::delete();
-Complex::update();
