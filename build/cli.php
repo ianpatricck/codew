c03c7e4f9712b0ead7180113c3b93813b5360693
Ian Patrick <56745915+ianpatricck@users.noreply.github.com>
2020-09-15 18:22:37 -0300

    createComplex()

--- build/cli.php
+++ build/cli.php
@@ -36,10 +36,31 @@ function createController($argv, $name)
     }
 }
 
-function complex($argv)
+function createComplex($argv)
 {
-    if ($argv == 'complex') {
+    if ($argv == 'create:complex') {
+        $complex = 'vendor/codew/complex/';
 
-        // Coding ..
+        $dashboard  =       $complex . 'dashboard.view.php';
+        $register   =       $complex . 'register.view.php';
+        $login      =       $complex . 'login.view.php';
+        $update     =       $complex . 'update.view.php';
+
+        $index      =       $complex . 'index.php';
+        $controller =       $complex . 'Complex.php';
+        $css        =       $complex . 'complex.css';
+        $sql        =       $complex . 'complex.sql';
+
+        copy($dashboard, 'view/dashboard.view.php');
+        copy($register, 'view/register.view.php');
+        copy($login, 'view/login.view.php');
+        copy($update, 'view/update.view.php');
+        copy($index, 'index.php');
+        copy($controller, 'app/Controllers/Complex.php');
+        copy($css, 'assets/css/complex.css');
+        copy($sql, 'config/complex.sql');
+
+        echo "\nComplex successfully created\n";
+
     }
 }
