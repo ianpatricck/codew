fc6928582ef3b621b16ccc54f0554a060466520e
Ian Patrick <56745915+ianpatricck@users.noreply.github.com>
2020-09-16 15:19:46 -0300

    Controllers Complex

--- build/cli.php
+++ build/cli.php
@@ -41,22 +41,20 @@ function createComplex($argv)
     if ($argv == 'create:complex') {
         $complex = 'vendor/codew/complex/';
 
-        $dashboard  =       $complex . 'dashboard.view.php';
-        $register   =       $complex . 'register.view.php';
-        $login      =       $complex . 'login.view.php';
-        $update     =       $complex . 'update.view.php';
+        $dashboard          =       $complex . 'dashboard.view.php';
+        $register           =       $complex . 'register.view.php';
+        $login              =       $complex . 'login.view.php';
+        $update             =       $complex . 'update.view.php';
 
-        $index      =       $complex . 'index.php';
-        $controller =       $complex . 'Complex.php';
-        $css        =       $complex . 'complex.css';
-        $sql        =       $complex . 'complex.sql';
+        $index              =       $complex . 'index.php';
+        $css                =       $complex . 'complex.css';
+        $sql                =       $complex . 'complex.sql';
 
         copy($dashboard, 'view/dashboard.view.php');
         copy($register, 'view/register.view.php');
         copy($login, 'view/login.view.php');
         copy($update, 'view/update.view.php');
         copy($index, 'index.php');
-        copy($controller, 'app/Controllers/Complex.php');
         copy($css, 'assets/css/complex.css');
         copy($sql, 'config/complex.sql');
 
@@ -90,6 +88,8 @@ function removeComplex($argv)
 
 function createComplexDB($argv)
 {
+    $complex = 'vendor/codew/complex/';
+
     if ($argv == 'create:mysql:complex') {
         $conn = new PDO('mysql:host='.HOST, USERNAME, PASSWORD, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
         
@@ -97,7 +97,11 @@ function createComplexDB($argv)
 
         $stmt = $conn->prepare($complex_sql);
         $stmt->execute();
+        
+        $controllerMySQL    =       $complex . 'ControllerMySQL.php';
 
+        copy($controllerMySQL, 'app/Controllers/Complex.php');
+        
         echo "\nCodew database created\n";
     } elseif ($argv == 'create:pgsql:complex') {
         $conn = new PDO('pgsql:host='.HOST, USERNAME, PASSWORD, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
@@ -106,6 +110,10 @@ function createComplexDB($argv)
 
         $stmt = $conn->prepare($complex_sql);
         $stmt->execute();
+
+        $controllerPgSQL    =       $complex . 'ControllerPgSQL.php';
+
+        copy($controllerPgSQL, 'app/Controllers/Complex.php');
 
         echo "\nCodew database created\n";
     }
