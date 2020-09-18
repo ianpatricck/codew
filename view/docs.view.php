643f3439f4656902fcb9329c12e9255c716fd828
Ian Patrick <56745915+ianpatricck@users.noreply.github.com>
2020-09-18 10:58:35 -0300

    right-side from docs

--- view/docs.view.php
+++ view/docs.view.php
@@ -7,7 +7,21 @@
     <link rel="stylesheet" href="<?= assets('css/style.css'); ?>">
 </head>
 <body>
-    
+    <div class="right-side">
+        <ul>
+            <a class="docs-link" href="#"><li>Config</li></a>
+            <a class="docs-link" href="#"><li>App</li></a>
+            <a class="docs-link" href="#"><li>CLI</li></a>
+            <a class="docs-link" href="#"><li>Internal functions</li></a>
+            <a class="docs-link" href="#"><li>URL</li></a>
+            <a class="docs-link" href="#"><li>Inputs</li></a>
+            <a class="docs-link" href="#"><li>Requests</li></a>
+            <a class="docs-link" href="#"><li>Sessions</li></a>
+            <a class="docs-link" href="#"><li>Database</li></a>
+            <a class="docs-link" href="#"><li>Complex</li></a>
+        </ul>
+    </div>
+
     <script src="<?= assets('js/script.js'); ?>"></script>
 </body>
 </html>
