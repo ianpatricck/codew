f7d5867800de0be786ea5d0d91d3925f5f5033f6
Ian Patrick <56745915+ianpatricck@users.noreply.github.com>
2020-09-19 16:59:06 -0300

    Connection class

---
+++ app/Native/Connection.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Native;
+
+use PDO;
+use PDOException;
+
+abstract class Connection
+{
+    protected $connection;
+
+    public function __construct($database)
+    {
+        if ($database == 'mysql') {
+            try {
+                $this->connection = new PDO(
+                    'mysql:host='.HOST.';dbname='.DB_NAME, USERNAME, PASSWORD, 
+                    array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
+                );
+
+                $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $this->connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+            } catch (PDOException $error) {
+                throw new PDOException($error);
+            }
+        } else if ($database == 'pgsql') {
+            try {
+                $this->connection = new PDO('pgsql:host='.HOST.';dbname='.DB_NAME, USERNAME, PASSWORD);
+
+                $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $this->connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+            } catch (PDOException $error) {
+                throw new PDOException($error);
+            }
+        }
+    }
+}
--- app/Native/DB.php
+++ app/Native/DB.php
@@ -5,38 +5,15 @@ namespace App\Native;
 use PDO;
 use PDOException;
 
-class DB
+class DB extends Connection
 {
-    private $connection;
-
-    public function __construct($database)
-    {
-        if ($database == 'mysql') {
-            try {
-                $this->connection = new PDO('mysql:host='.HOST.';dbname='.DB_NAME, USERNAME, PASSWORD, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-                $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                $this->connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
-            } catch (PDOException $error) {
-                throw new PDOException($error);
-            }
-        } else if ($database == 'pgsql') {
-            try {
-                $this->$connection = new PDO('pgsql:host='.HOST.';dbname='.DB_NAME, USERNAME, PASSWORD);
-                $this->$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                $this->connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
-            } catch (PDOException $error) {
-                throw new PDOException($error);
-            }
-        }
-    }
-
-    public function InsertQuery($query)
+    public function Insert($query)
     {
         $stmt = $this->connection->prepare($query);
         $stmt->execute();
     }
 
-    public function returnQuery($query)
+    public function return($query)
     {
         $stmt = $this->connection->prepare($query);
         $stmt->execute();
@@ -44,7 +21,7 @@ class DB
         return $stmt->fetch();
     }
 
-    public function returnQueryAll($query)
+    public function returnAll($query)
     {
         $stmt = $this->connection->prepare($query);
         $stmt->execute();
