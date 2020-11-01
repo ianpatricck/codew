30fc10088a950ff0fb02bddbbc265897bd3a9470
Ian Patrick <56745915+ianpatricck@users.noreply.github.com>
2020-11-01 15:40:56 -0300

    view path

--- src/HTML.php
+++ src/HTML.php
@@ -6,6 +6,6 @@ class HTML
 {
     public static function view($page, $var = [])
     {
-        require_once __DIR__ . '/../../../../' . $page . '.view.php';
+        require_once __DIR__ . '/../../../../views/' . $page . '.view.php';
     }
 }
