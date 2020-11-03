606e387450d4aa7c6f90dcdd6cda7625d91d9555
Ian Patrick <56745915+ianpatricck@users.noreply.github.com>
2020-11-03 16:01:55 -0300

    Connection updated

--- src/Connection.php
+++ src/Connection.php
@@ -23,23 +23,20 @@ abstract class Connection
     {
         if ($database == 'mysql') {
             try {
-                /*
                 $this->connection = new PDO(
-                    'mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_USERNAME, DB_PASSWORD, 
+                    'mysql:host='.$params['host'].';dbname='.$params['dbname'], $params['username'], $params['password'], 
                     array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
                 );
                 
                 $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 $this->connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
-                */
 
-                
             } catch (PDOException $error) {
                 throw new PDOException($error);
             }
         } else if ($database == 'pgsql') {
             try {
-                $this->connection = new PDO('pgsql:host='.DB_HOST.';dbname='.DB_NAME, DB_USERNAME, DB_PASSWORD);
+                $this->connection = new PDO('pgsql:host='.$params['host'].';dbname='.$params['dbname'], $params['username'], $params['password']);
                 $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 $this->connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
             } catch (PDOException $error) {
