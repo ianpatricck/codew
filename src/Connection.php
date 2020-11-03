52dab1c1e49d7cf06167bbc7a8a71e03ff943b6a
Ian Patrick <56745915+ianpatricck@users.noreply.github.com>
2020-11-03 09:01:13 -0300

    Testing connection

--- src/Connection.php
+++ src/Connection.php
@@ -19,24 +19,27 @@ abstract class Connection
         return $this->connection;
     }
 
-    public function __construct($database)
+    public function __construct($database, $params = [])
     {
         if ($database == 'mysql') {
             try {
+                /*
                 $this->connection = new PDO(
                     'mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_USERNAME, DB_PASSWORD, 
                     array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
                 );
-
+                
                 $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 $this->connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+                */
+
+                
             } catch (PDOException $error) {
                 throw new PDOException($error);
             }
         } else if ($database == 'pgsql') {
             try {
                 $this->connection = new PDO('pgsql:host='.DB_HOST.';dbname='.DB_NAME, DB_USERNAME, DB_PASSWORD);
-
                 $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 $this->connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
             } catch (PDOException $error) {
