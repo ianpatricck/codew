8d898e6476af6ba214eb45b351d080d71a0b4a49
Ian Patrick <56745915+ianpatricck@users.noreply.github.com>
2020-11-09 19:12:57 -0300

    View::view()

--- src/View.php
+++ src/View.php
@@ -4,5 +4,9 @@ namespace Native;
 
 class View extends \League\Plates\Engine
 {
-    // ..
+    public static function view($file, $data = [])
+    {
+        $view = new \League\Plates\Engine("views");
+        echo $view->render($file, $data);
+    }
 }
