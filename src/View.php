d5e8f683074170a1047f4efff3c864074618cbc6
Ian Patrick <56745915+ianpatricck@users.noreply.github.com>
2020-11-09 16:42:37 -0300

    Removed content class

--- src/View.php
+++ src/View.php
@@ -4,10 +4,5 @@ namespace Native;
 
 class View extends \League\Plates\Engine
 {
-    private $instance;
-
-    public function render($view, $parameters = [])
-    {
-        echo $this->instance->render($view, $parameters);
-    }
+    // ..
 }
