7c55b41bb3e47ad0a316c84fc1a9492f528d72e5
Ian Patrick <56745915+ianpatricck@users.noreply.github.com>
2020-11-09 16:37:05 -0300

    Removed constructor

--- src/View.php
+++ src/View.php
@@ -6,11 +6,6 @@ class View extends \League\Plates\Engine
 {
     private $instance;
 
-    public function __construct($path)
-    {
-        $this->instance = new League\Plates\Engine($path);
-    }
-
     public function render($view, $parameters = [])
     {
         echo $this->instance->render($view, $parameters);
