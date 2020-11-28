5795a997a330d9965dd9acb911b1c41dd6e911f9
Ian Patrick <56745915+ianpatricck@users.noreply.github.com>
2020-11-27 23:29:07 -0300

    colors and conditions

--- src/Codew.php
+++ src/Codew.php
@@ -2,15 +2,17 @@
 
 namespace Codew;
 
+require_once __DIR__ . '/content/file.php';
+
 class Codew
 {
     public function run($args)
-    {       
-        if (in_array('-c', $args) && substr($args[1], -4) == '.pcp') {
-            echo 'true';
+    {
+        if (in_array('-c', $args) && substr($args[1], -4) == '.pcp' && substr($args[3], -4) == '.php') {
+            compile($args[1], $args[3]);
         } else {
             $climate = new \League\CLImate\CLImate;
-            echo $climate->red("Failed to compile file: invalid extension or invalid command syntax.");
+            $climate->red('Failed to compile file: invalid extension or invalid command syntax');
         }
     }
 }
--- src/content/file.php
+++ src/content/file.php
@@ -2,5 +2,6 @@
 
 function compile($from, $to)
 {
-    //
+    echo $from . "\n";
+    echo $to;
 }
