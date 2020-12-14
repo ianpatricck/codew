0b5b9637af9298166d82168bc789210c117622b8
Ian Patrick <56745915+ianpatricck@users.noreply.github.com>
2020-12-14 20:29:57 -0300

    Update reduce ; methods

--- src/content/index.php
+++ src/content/index.php
@@ -10,17 +10,25 @@ function compile($from, $to)
     while (!feof($fpcp)) {
         $content = fgets($fpcp);
         $newContent = [];
-    
-        if (preg_match('/echo/', $content)) {
+
+        if (preg_match('/echo/', $content) || preg_match('/\$/', $content)) {
             $explode = explode("\n", rtrim($content));
-            
+
             foreach ($explode as $key => $value) {
-                array_push($newContent, $value . ";\n");
+                if (!preg_match('/;/', $value)) {
+                    array_push($newContent, $value . ";\n");
+                }
             }
-    
+
             $content = str_replace($content, implode($newContent), $content);
         }
-    
+
+        /*
+        if (!preg_match('/function/', $content) && preg_match('/()/', $content)) {
+            echo $content;
+        }
+        */
+
         fwrite($fphp, $content);
     }
 
