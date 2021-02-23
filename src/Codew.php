16fa8d36f7c60ea855bfb69ac013e42d9250839f
Ian Patrick <56745915+ianpatricck@users.noreply.github.com>
2021-02-22 23:32:22 -0300

    Compiler class

--- src/Codew.php
+++ src/Codew.php
@@ -4,16 +4,19 @@ namespace Codew;
 
 require_once __DIR__ . '/core/compiler.php';
 
+use Codew\Compiler;
+
 class Codew
 {
     public function run($args)
     {
         $climate = new \League\CLImate\CLImate;
+        $compiler = new \Codew\Compiler;
 
         if (in_array('-c', $args) && substr($args[1], -4) == '.pcp' && substr($args[3], -4) == '.php') {
-            compileFile($args[1], $args[3]);
+            $compiler->compileFile($args[1], $args[3]);
         } else if (in_array('-c', $args) && $args[1] == '-c') {
-            compileDirectory($args[2]);
+            $compiler->compileDirectory($args[2]);
         } else {
             $climate->red('Failed to compile');
         }
--- src/core/compiler.php
+++ src/core/compiler.php
@@ -5,106 +5,111 @@
  * 
  */
 
+namespace Codew;
+
 require_once __DIR__ . '/code/sintax.php';
 require_once __DIR__ . '/code/repeat.php';
 
-function compileFile($from, $to)
+class Compiler
 {
-    $fpcp = fopen($from, 'r');
-    $fphp = fopen($to, 'wb');
-
-    fwrite($fphp, "<?php\n\n");
-
-    while (!feof($fpcp)) {
-        $content = fgets($fpcp);
-        $newContent = [];
-
-        if (importFromDirectory($content)) {
-            $explode = explode(' ', $content);
-
-            if ($explode[0] == "") {
-                $explodeFinal = array_merge(array_filter($explode), []);
-                $content = str_replace($content, implode($newContent), implode(tabs($explode)) . "foreach (glob(" . rtrim(rtrim($explodeFinal[2]), '\';') ."/*.php') { require __DIR__ . '\$value'; }");
-            } else {
-                $content = str_replace($content, implode($newContent), "foreach (glob(" . rtrim(rtrim($explode[2]), '\';') ."/*.php') as \$value) { require __DIR__ . \"/\$value\"; } \n");
-            }
-        }
-
-        if (for_in($content)) {
-            $explode = explode(' ', $content);
-
-            if ($explode[0] == "") {
-                $explodeFinal = array_merge(array_filter($explode), []);
-                $content = str_replace($content, implode($newContent), implode(tabs($explode)) . 'foreach (' . $explodeFinal[3] . ' as ' . $explodeFinal[1] . ") {\n");
-            } else {
-                $content = str_replace($content, implode($newContent), 'foreach (' . $explode[3] . ' as ' . $explode[1] . ") {\n");
-            }
-        }
-
-        if (mysql_pdo($content)) {
-            $explodeInitial = explode(' = ', $content);
-            $explodeMiddle = explode('MYSQL_PDO(', $explodeInitial[1]);
-            $explodeFinal = explode(');', $explodeMiddle[1]);
-            $explode = explode(', ', $explodeFinal[0]);
-            
-            $host = str_replace(array('\'', '"'), '', $explode[0]);
-            $dbname = str_replace(array('\'', '"'), '', $explode[1]);
-            $username = $explode[2];
-            $password = $explode[3];
-
-            $content = str_replace(
-                $content, 
-                implode($newContent), 
-
-                $explodeInitial[0] . " = new PDO(\"mysql:host=$host;dbname=$dbname\", $username, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));\n" .
-                rtrim($explodeInitial[0]) . "->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);\n" .
-                rtrim($explodeInitial[0]) . "->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);\n"
-            );
-        }
-
-        if (pgsql_pdo($content)) {
-            $explodeInitial = explode(' = ', $content);
-            $explodeMiddle = explode('PGSQL_PDO(', $explodeInitial[1]);
-            $explodeFinal = explode(');', $explodeMiddle[1]);
-            $explode = explode(', ', $explodeFinal[0]);
-            
-            $host = str_replace(array('\'', '"'), '', $explode[0]);
-            $dbname = str_replace(array('\'', '"'), '', $explode[1]);
-            $username = $explode[2];
-            $password = $explode[3];
-
-            $content = str_replace(
-                $content, 
-                implode($newContent), 
-
-                $explodeInitial[0] . " = new PDO(\"pgsql:host=$host;dbname=$dbname\", $username, $password);\n" .
-                rtrim($explodeInitial[0]) . "->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);\n" .
-                rtrim($explodeInitial[0]) . "->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);\n"
-            );
-        }
-        
-        fwrite($fphp, $content);
-    }
-
-    fclose($fpcp);
-    fclose($fphp);
-}
-
-function compileDirectory($directory)
-{
-    $structure = new RecursiveTreeIterator(new RecursiveDirectoryIterator($directory, RecursiveDirectoryIterator::SKIP_DOTS));
+    public function compileFile($from, $to)
+    {
+        $fpcp = fopen($from, 'r');
+        $fphp = fopen($to, 'wb');
     
-    foreach($structure as $path) {
-        $path = ltrim($path, '\|- ');
-
-        if (substr($path, -4) == '.pcp') {
-            $fphp = str_replace('.pcp', '.php', $path);
-            compileFile($path, $fphp);
+        fwrite($fphp, "<?php\n\n");
+    
+        while (!feof($fpcp)) {
+            $content = fgets($fpcp);
+            $newContent = [];
+    
+            if (importFromDirectory($content)) {
+                $explode = explode(' ', $content);
+    
+                if ($explode[0] == "") {
+                    $explodeFinal = array_merge(array_filter($explode), []);
+                    $content = str_replace($content, implode($newContent), implode(tabs($explode)) . "foreach (glob(" . rtrim(rtrim($explodeFinal[2]), '\';') ."/*.php') { require __DIR__ . '\$value'; }");
+                } else {
+                    $content = str_replace($content, implode($newContent), "foreach (glob(" . rtrim(rtrim($explode[2]), '\';') ."/*.php') as \$value) { require __DIR__ . \"/\$value\"; } \n");
+                }
+            }
+    
+            if (for_in($content)) {
+                $explode = explode(' ', $content);
+    
+                if ($explode[0] == "") {
+                    $explodeFinal = array_merge(array_filter($explode), []);
+                    $content = str_replace($content, implode($newContent), implode(tabs($explode)) . 'foreach (' . $explodeFinal[3] . ' as ' . $explodeFinal[1] . ") {\n");
+                } else {
+                    $content = str_replace($content, implode($newContent), 'foreach (' . $explode[3] . ' as ' . $explode[1] . ") {\n");
+                }
+            }
+    
+            if (mysql_pdo($content)) {
+                $explodeInitial = explode(' = ', $content);
+                $explodeMiddle = explode('MYSQL_PDO(', $explodeInitial[1]);
+                $explodeFinal = explode(');', $explodeMiddle[1]);
+                $explode = explode(', ', $explodeFinal[0]);
+                
+                $host = str_replace(array('\'', '"'), '', $explode[0]);
+                $dbname = str_replace(array('\'', '"'), '', $explode[1]);
+                $username = $explode[2];
+                $password = $explode[3];
+    
+                $content = str_replace(
+                    $content, 
+                    implode($newContent), 
+    
+                    $explodeInitial[0] . " = new PDO(\"mysql:host=$host;dbname=$dbname\", $username, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));\n" .
+                    rtrim($explodeInitial[0]) . "->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);\n" .
+                    rtrim($explodeInitial[0]) . "->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);\n"
+                );
+            }
+    
+            if (pgsql_pdo($content)) {
+                $explodeInitial = explode(' = ', $content);
+                $explodeMiddle = explode('PGSQL_PDO(', $explodeInitial[1]);
+                $explodeFinal = explode(');', $explodeMiddle[1]);
+                $explode = explode(', ', $explodeFinal[0]);
+                
+                $host = str_replace(array('\'', '"'), '', $explode[0]);
+                $dbname = str_replace(array('\'', '"'), '', $explode[1]);
+                $username = $explode[2];
+                $password = $explode[3];
+    
+                $content = str_replace(
+                    $content, 
+                    implode($newContent), 
+    
+                    $explodeInitial[0] . " = new PDO(\"pgsql:host=$host;dbname=$dbname\", $username, $password);\n" .
+                    rtrim($explodeInitial[0]) . "->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);\n" .
+                    rtrim($explodeInitial[0]) . "->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);\n"
+                );
+            }
+            
+            fwrite($fphp, $content);
+        }
+    
+        fclose($fpcp);
+        fclose($fphp);
+    }
+    
+    public function compileDirectory($directory)
+    {
+        $structure = new RecursiveTreeIterator(new RecursiveDirectoryIterator($directory, RecursiveDirectoryIterator::SKIP_DOTS));
+        
+        foreach($structure as $path) {
+            $path = ltrim($path, '\|- ');
+    
+            if (substr($path, -4) == '.pcp') {
+                $fphp = str_replace('.pcp', '.php', $path);
+                $this->compileFile($path, $fphp);
+            }
         }
     }
-}
-
-function compileToDirectory($directory)
-{
-    // ..
+    
+    public function compileToDirectory($directory)
+    {
+        // ..
+    }
 }
