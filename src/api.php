38fdd687d47b0ecd21db88a8779699ce14472725
Ian Patrick <56745915+ianpatricck@users.noreply.github.com>
2021-03-18 22:44:15 -0300

    API response method

--- src/api.php
+++ src/api.php
@@ -1,3 +1,11 @@
 <?php
 
-// ..
+namespace Codew;
+
+class API
+{
+    public static function response($arr)
+    {
+        echo json_encode($arr);
+    }
+}
--- src/res.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-function response($arr)
-{
-    echo json_encode($arr);
-}
