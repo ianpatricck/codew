0b8a0e6ab7097d92af89af6ca1eb780e52afb7dd
Ian Patrick <56745915+ianpatricck@users.noreply.github.com>
2021-03-26 20:24:12 -0300

    get method | response method description

--- src/api.php
+++ src/api.php
@@ -4,8 +4,34 @@ namespace Codew;
 
 class API
 {
+    /**
+     * Response method (JSON)
+     * 
+     * @param array
+     * @return
+     */
+
     public static function response($arr)
     {
         echo json_encode($arr);
+    }
+
+    public static function get($url, $callback = null)
+    {   
+        $curl = curl_init();
+
+        curl_setopt_array($curl, [
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_URL => $url
+        ]);
+
+        if ($callback) {
+            $response = curl_exec($curl);
+            $callback($response);
+        } else {
+            return curl_exec($curl);
+        }
+
+        curl_close($curl);
     }
 }
