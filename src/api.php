08e12a29fcb8b4932a0c8df709f1450c135820e2
Ian Patrick <56745915+ianpatricck@users.noreply.github.com>
2021-04-01 21:21:18 -0300

    POST Request

--- src/api.php
+++ src/api.php
@@ -12,9 +12,9 @@ class API
      * 
      */
 
-    public static function response($arr)
+    public static function response($data)
     {
-        echo json_encode($arr);
+        echo json_encode($data);
     }
 
     /**
@@ -32,6 +32,33 @@ class API
         curl_setopt_array($curl, [
             CURLOPT_RETURNTRANSFER => 1,
             CURLOPT_URL => $url
+        ]);
+
+        $output = curl_exec($curl);
+
+        curl_close($curl);
+
+        return json_decode($output);
+    }
+
+    /**
+     * Request POST method (JSON)
+     * 
+     * @param string
+     * @param array
+     * @return string
+     * 
+     */
+
+    public static function post($url, $data = [])
+    {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, [
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_URL => $url,
+            CURLOPT_POST => true,
+            CURLOPT_POSTFIELDS => $data
         ]);
 
         $output = curl_exec($curl);
