3bf9d3a8d535c68dce3cc5b6db15e8ffd4a16ee9
Ian Patrick <56745915+ianpatricck@users.noreply.github.com>
2021-04-10 00:36:49 -0300

    DB select method

--- src/database/DB.php
+++ src/database/DB.php
@@ -6,5 +6,23 @@ use Codew\Database\Connection;
 
 class DB extends Connection
 {
-    // ..
+    public function select($query = [])
+    {
+
+        foreach ($query as $key => $table);
+        
+        $stmt = $this->connection->prepare("SELECT {$key} FROM {$table}");
+        $stmt->execute();
+
+        if ($key !== '*') {
+            return $stmt->fetch();
+        }
+        
+        return $stmt->fetchAll();
+    }
+
+    public function where($query = [])
+    {
+        // ..
+    }
 }
