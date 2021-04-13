1862b9573db99e72ba47c852269a9a4e42c57519
Ian Patrick <56745915+ianpatricck@users.noreply.github.com>
2021-04-13 19:13:20 -0300

    SELECT | WHERE | EXECUTE | FETCH

--- src/database/DB.php
+++ src/database/DB.php
@@ -6,23 +6,63 @@ use Codew\Database\Connection;
 
 class DB extends Connection
 {
-    public function select($query = [])
-    {
+    private $stmt;
+    private array $query;
 
-        foreach ($query as $key => $table);
-        
-        $stmt = $this->connection->prepare("SELECT {$key} FROM {$table}");
-        $stmt->execute();
-
-        if ($key !== '*') {
-            return $stmt->fetch();
-        }
-        
-        return $stmt->fetchAll();
-    }
-
-    public function where($query = [])
+    public function query($query)
     {
         // ..
+    }
+
+    public function select($condition = [])
+    {
+        foreach ($condition as $key => $table);
+
+        $this->query = [];
+
+        array_push($this->query, "SELECT {$key} FROM {$table}");
+        
+        return $this;
+    }
+
+    public function where($condition = [])
+    {
+        foreach ($condition as $key => $value);
+
+        array_push($this->query, "WHERE {$key} = {$value}");
+
+        return $this;
+    }
+
+    /**
+     * Execute query
+     * 
+     * @return array
+     * 
+     */
+
+    public function execute()
+    {
+        $this->stmt = $this->connection->prepare(implode(' ', $this->query));
+        $this->stmt->execute();
+
+        return $this;
+    }
+
+    /**
+     * Return data
+     * 
+     * @param string
+     * @return array
+     * 
+     */
+
+    public function fetch($all = null)
+    {
+        if ($all == 'all') {
+            return $this->stmt->fetchAll();
+        }
+        
+        return $this->stmt->fetch();
     }
 }
