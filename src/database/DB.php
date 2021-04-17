9d2ecf71467b134d65bb46db95d10fb052fc1757
Ian Patrick <56745915+ianpatricck@users.noreply.github.com>
2021-04-17 09:36:21 -0300

    Removed queries

--- src/database/DB.php
+++ src/database/DB.php
@@ -9,6 +9,15 @@ class DB extends Connection
     private $stmt;
     private array $query;
 
+    /**
+     * Query method
+     * 
+     * @param string $query
+     * @param array $bindings
+     * @return $this
+     * 
+     */
+
     public function query($query, $bindings = [])
     {
         $this->query = [];
@@ -18,45 +27,6 @@ class DB extends Connection
         $this->stmt = $this->connection->prepare(implode(' ', $this->query));
         $this->stmt->execute();
         
-        return $this;
-    }
-
-    public function insert($table, $query)
-    {
-        $table_values = [];
-        $values = [];
-
-        foreach($query as $key => $value) {
-            array_push($table_values, $key);
-        }
-
-        foreach($query as $value) {
-            array_push($values, "\"$value\"");
-        }
-
-        $query = ["INSERT INTO {$table}(" . implode(', ', $table_values) . ") VALUES (" . implode(', ', $values) . ");"];        
-
-        $this->stmt = $this->connection->prepare(implode($query));
-        $this->stmt->execute();
-    }
-
-    public function select($condition = [])
-    {
-        foreach ($condition as $key => $table);
-
-        $this->query = [];
-
-        array_push($this->query, "SELECT {$key} FROM {$table}");
-        
-        return $this;
-    }
-
-    public function where($condition = [])
-    {
-        foreach ($condition as $key => $value);
-
-        array_push($this->query, "WHERE {$key} = {$value}");
-
         return $this;
     }
 
